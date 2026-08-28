926f11d09c84eab7066ff38b4b16d30378b4cd9f
shairyhafsa-ux <shairyhafsa-ux-shairyhafsa14@gmail.com>
2026-08-28 19:38:56 +0600

    abstract syntax tree header file contributed by Hafsa

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,16 @@
 #include "../include/lexer.h"
 #include "../include/parser.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 int main() {
+#ifdef _WIN32
+    // Force Windows Terminal / PowerShell to render UTF-8 box-drawing characters
+    SetConsoleOutputCP(CP_UTF8);
+#endif
+
     std::string sampleCode = 
         "define total as number with 10 + 5 * 2;\n"
         "output(total);";
@@ -18,7 +27,13 @@ int main() {
     std::cout << "\n--- Parsing ---\n";
     Parser parser(tokens);
     auto ast = parser.parseProgram();
+
     std::cout << "Successfully parsed " << ast->statements.size() << " top-level statement(s).\n";
+
+    if (ast) {
+        std::cout << "\n--- Parse Tree ---\n";
+        ast->printTree();
+    }
 
     return 0;
 }
