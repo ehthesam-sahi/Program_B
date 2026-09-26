58ce70163f30a821d3dbedd355d8666b8e493fbc
shairyhafsa-ux <shairyhafsa-ux-shairyhafsa14@gmail.com>
2026-09-26 11:36:17 +0600

    webassembly header file

---
+++ include/codegen_wasm.h
@@ -0,0 +1,27 @@
+#ifndef CODEGEN_WASM_H
+#define CODEGEN_WASM_H
+
+#include "ast.h"
+#include <string>
+#include <sstream>
+
+class WasmCodeGenerator {
+private:
+    std::ostringstream wat;
+    int indentLevel;
+
+    void indent();
+    void generateNode(const ASTNode* node);
+    void generateVarDecl(const VarDeclNode* node);
+    void generateAssign(const AssignNode* node);
+    void generatePrint(const PrintNode* node);
+    void generateIf(const IfNode* node);
+    void generateWhile(const WhileNode* node);
+    void generateExpr(const ASTNode* expr);
+
+public:
+    WasmCodeGenerator() : indentLevel(0) {}
+    std::string generate(const ProgramNode* program);
+};
+
+#endif
